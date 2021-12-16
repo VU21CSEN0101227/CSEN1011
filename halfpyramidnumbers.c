f39977c8287ae7b5e7c3853b629910e8c0bd0b0c
thekilari <93367713+thekilari@users.noreply.github.com>
2021-12-16 14:12:10 +0530

    Add files via upload

---
+++ halfpyramidnumbers.c
@@ -0,0 +1,21 @@
+// half pyramid of numbers
+#include<stdio.h>
+int main()
+{
+	int i,j,r;
+	printf("Enter number of rows:");
+	scanf("%d",&r);
+	for(i=1;i<=r;++i)
+	{
+		for(j=1;j<=i;++j)
+		{
+			printf("%d",j);
+		}
+		printf("\n");	
+	}
+}
+/*output:
+enter number of rows:3
+1
+12
+123*/
